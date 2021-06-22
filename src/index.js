03d2b9140a27de0c0d9707d3c829f02e8a2602c5
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-06-22 15:12:50 -0300

    fix: update toast properties

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './styles/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <ToastContainer />
+    <ToastContainer
+      position={'top-center'}
+      autoClose={3000}
+      hideProgressBar
+      closeOnClick
+      pauseOnHover
+      draggable
+      limit={1}
+      transition={Slide}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
