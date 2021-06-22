941aab0ac1d3251e03d40a55e2e818c96b72d663
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-06-22 15:13:00 -0300

    feat: add loader to button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,12 @@
-export const Button = ({ children, type, onClick, disabled, color, transparent }) => (
+import Loader from 'react-loader-spinner';
+
+export const Button = ({ children, type, onClick, disabled, color, transparent, isLoading }) => (
   <button
     disabled={disabled}
     type={type || 'button'}
-    className={`btn btn--${color ? color : ''} btn--${transparent}`}
+    className={`btn btn--${color ? color : ''} btn--${transparent ? transparent : ''}`}
     onClick={onClick || null}
   >
-    {children}
+    {false ? <Loader type="Oval" color="#fff" height={18} width={18} visible={true} /> : children}
   </button>
 );
